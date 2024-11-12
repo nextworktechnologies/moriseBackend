a6b8a968fb6d44975e619877ed6436452949c8c0
Dev-ChandanTomar <chandan.nextwork@gmail.com>
2024-11-12 18:21:53 +0530

    added all collections

--- Collections/collections.js
+++ Collections/collections.js
@@ -1,7 +1,16 @@
 import { client } from "../dbConnection.js";
 const collections = {
-  documentCollection: () =>
-    client.db(process.env.MONGO_DATABASE).collection("documents"),
+  transCollection: () => client.db(process.env.MONGO_DATABASE).collection("transactions"),
+  documentCollection: () =>client.db(process.env.MONGO_DATABASE).collection("documents"),
+  categoryCollection: () =>client.db(process.env.MONGO_DATABASE).collection("categoy"),
+  mediaCollection: () =>client.db(process.env.MONGO_DATABASE).collection("media"),
+  userCollection: () => client.db(process.env.MONGO_DATABASE).collection("users"),
+  addressCollection: () => client.db(process.env.MONGO_DATABASE).collection("address"),
+  notifCollection: () => client.db(process.env.MONGO_DATABASE).collection("notifications"),
+  occupationCollection: () => client.db(process.env.MONGO_DATABASE).collection("occupation"),
+  qualificationCollection: () => client.db(process.env.MONGO_DATABASE).collection("qualification"),
+  queryCollection: () => client.db(process.env.MONGO_DATABASE).collection("query"),
+  sourceCollection: () => client.db(process.env.MONGO_DATABASE).collection("source"),
 };
 
 export default collections;
