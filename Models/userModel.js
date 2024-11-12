bb33ce96e04716a53fd9b734bc7f9d8540e3b375
Dev-ChandanTomar <chandan.nextwork@gmail.com>
2024-11-12 16:18:17 +0530

    user model created

---
+++ Models/userModel.js
@@ -0,0 +1,137 @@
+class UserModel {
+    constructor(
+      userId,
+      fullName,
+      password,
+      sponsorId,
+      email,
+      phone,
+      image,
+      dob,
+      status,
+      role,
+      isVerified,
+      rewardId,
+      createdAt,
+      updatedAt
+    ) {
+      this.userId = userId;
+      this.fullName = fullName;
+      this.password = password;
+      this.sponsorId = sponsorId;
+      this.email = email;
+      this.phone = phone;
+      this.image = image;
+      this.dob = dob;
+      this.status = status;
+      this.role = role;
+      this.isVerified = isVerified;
+      this.rewardId = rewardId;
+      this.createdAt = createdAt;
+      this.updatedAt = updatedAt;
+    }
+  
+    fromJson(jsonData) {
+      return new UserModel(
+        jsonData.userId,
+        jsonData.fullName ?? "Not Available",
+        jsonData.password,
+        jsonData.sponsorId.toLowerCase() ?? "",
+        jsonData.email ?? "",
+        jsonData.phone,
+        jsonData?.image ?? "",
+        jsonData.dob ?? "dd-mm-yyyy",
+        jsonData.status != null ? JSON.parse(jsonData.status) : false,
+        jsonData.role ?? "individual",
+        jsonData?.isVerified != null ? JSON.parse(jsonData.isVerified) : false,
+        jsonData.rewardId ?? [],
+        jsonData.createdAt ?? new Date(),
+        jsonData.updatedAt ?? new Date()
+      );
+    }
+  
+  
+    // Function to convert User instance to a JSON object suitable for database insertion
+    toDatabaseJson() {
+      return {
+        userId: this.userId,
+        fullName: this.fullName,
+        password: this.password,
+        sponsorId: this.sponsorId,
+        email: this.email,
+        phone: this.phone,
+        image: this.image,
+        dob: this.dob,
+        status: this.status,
+        role: this.role,
+        isVerified: this.isVerified,
+        rewardId: this.rewardId,
+        createdAt: this.createdAt,
+        updatedAt: this.updatedAt,
+      };
+    }
+  
+    // Data to client
+    toClientJson() {
+      return {
+        id: this.userId,
+        userId: this.userId,
+        fullName: this.fullName,
+        sponsorId: this.sponsorId,
+        email: this.email,
+        phone: this.phone,
+        image: this.image,
+        dob: this.dob,
+        status: this.status,
+        role: this.role,
+        isVerified: this.isVerified,
+        rewardId: this.rewardId,
+        createdAt: this.createdAt,
+        updatedAt: this.updatedAt,
+      };
+    }
+  
+    toMemberJson(e) {
+      return {
+        userId: e.userId,
+        id: e.userId,
+        fullName: e.fullName,
+        sponsorId: e.sponsorId,
+        email: e.email,
+        phone: e.phone,
+        status: e.status,
+        isVerified: e.isVerified,
+        createdAt: e.createdAt,
+      };
+    }
+  
+    toUpdateJson(body) {
+      const updateJson = {};
+  
+      for (const key in body) {
+        if (key !== "id" && this.hasOwnProperty(key) && body[key] !== undefined && body[key] !== "") {
+          let value = body[key];
+  
+          // Convert string representation of boolean to actual boolean
+          if (value === "true" || value === "false") {
+            value = value === "true";
+          }
+  
+          // Convert string representation of number to actual number
+          const parsedNumber = parseFloat(value);
+          if (!isNaN(parsedNumber)) {
+            value = parsedNumber;
+          }
+  
+          updateJson[key] = value;
+        }
+      }
+  
+      updateJson.updatedAt = new Date();
+      return updateJson;
+    }
+  
+  }
+  
+  export default UserModel;
+  
---
+++ index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import http from "http";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import "./dbConnection.js";
+import userRoutes from "./Routes/UserRoute.js";
+import cors from 'cors';
+
+const app = express();
+
+// Define a function to determine the allowed origin dynamically
+const AllowedOrigin = (origin, callback) => {
+    // Check if the origin is one of the allowed origins
+    const allowedOrigins = ['https://admin.knoone.com', 'https://acc.knoone.com', 'https://knoone.com', "http://localhost:3000", "http://localhost:5500", "http://localhost:3001", "http://192.168.0.243:3000", "http://192.168.0.193:3000", "http://192.168.0.193:3001", "http://192.168.0.150:3000", "http://192.168.1.7:3000"];
+    const isAllowed = allowedOrigins.includes(origin);
+  
+    // Call the callback with the result (error, isAllowed)
+    callback(null, isAllowed ? origin : null);
+  };
+  
+  // Use CORS middleware with dynamic origin determination
+  app.use(cors({
+    origin: AllowedOrigin,
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  }));
+
+  const port = process.env.PORT || 3000;
+
+// ... (rest of your middleware and routes)
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(compression());
+
+
+app.use("/api/v1", userRoutes);
+
+
+
+const server = http.createServer(app);
+// Set a timeout of 10 minutes (600000 milliseconds)
+server.setTimeout(1000000);
+
+(async () => {
+  try {
+    await server.listen(port); // Start the server
+    await connectToMongo();
+    console.log(`Server is running. (${port})`);
+  } catch (error) {
+    console.error("Error starting the server:", error);
+  }
+})();
